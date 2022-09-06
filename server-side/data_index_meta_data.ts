c6e4cbd9a320c7517dad6e5313a2f7be6c11a967
Shir Marciano <shir.o@pepperi.com>
2022-09-06 11:06:21 +0300

    Fix the schema object and multiStringValue fields

--- server-side/data_index_meta_data.ts
+++ server-side/data_index_meta_data.ts
@@ -3,14 +3,14 @@ import { CommonMethods } from './CommonMethods';
 import jwtDecode from "jwt-decode";
 import { PapiClient } from '@pepperi-addons/papi-sdk';
 
-/////function to remove - I change the interface, need to remove after a few versios
-export async function all_activities_fields(client: Client, request: Request): Promise<any> {
-    return await getDataIndexFields(client, "all_activities");
-}
-/////function to remove - I change the interface, need to remove after a few versios
-export async function transaction_lines_fields(client: Client, request: Request): Promise<any> {
-    return await getDataIndexFields(client, "transaction_lines");
-}
+// /////function to remove - I change the interface, need to remove after a few versios
+// export async function all_activities_fields(client: Client, request: Request): Promise<any> {
+//     return await getDataIndexFields(client, "all_activities");
+// }
+// /////function to remove - I change the interface, need to remove after a few versios
+// export async function transaction_lines_fields(client: Client, request: Request): Promise<any> {
+//     return await getDataIndexFields(client, "transaction_lines");
+// }
 
 export async function all_activities_schema(client: Client, request: Request): Promise<any> {
     return await getDataIndexSchema(client, "all_activities");
@@ -20,84 +20,75 @@ export async function transaction_lines_schema(client: Client, request: Request)
     return await getDataIndexSchema(client, "transaction_lines");
 }
 
-/////function to remove - I change the interface, need to remove after a few versios
-async function getDataIndexFields(client: Client, dataIndexType: string) {
-    var papiClient = CommonMethods.getPapiClient(client);
-    // need to take the fields we saved from the UI and the exported field because 
-    //it can be a case where the build is now immidialty and a code job will do it
-    // so we want to get the fields we save in the last time 
-    var ui_adalRecord = await CommonMethods.getDataIndexUIAdalRecord(papiClient,client);
+interface SchemaFields{
+    [key: string]: {Type:string, OptionalValues?:String[]};
     
-    var fields = [];
-    if (ui_adalRecord[`${dataIndexType}_fields`]) {
-        fields = ui_adalRecord[`${dataIndexType}_fields`];
-    }
-
-    return { Fields: fields };
 }
+
+/////function to remove - I change the interface, need to remove after a few versios
+// async function getDataIndexFields(client: Client, dataIndexType: string) {
+//     var papiClient = CommonMethods.getPapiClient(client);
+//     // need to take the fields we saved from the UI and the exported field because 
+//     //it can be a case where the build is now immidialty and a code job will do it
+//     // so we want to get the fields we save in the last time 
+//     var ui_adalRecord = await CommonMethods.getDataIndexUIAdalRecord(papiClient,client);
+    
+//     var fields = [];
+//     if (ui_adalRecord[`${dataIndexType}_fields`]) {
+//         fields = ui_adalRecord[`${dataIndexType}_fields`];
+//     }
+
+//     return { Fields: fields };
+// }
 
 async function getDataIndexSchema(client: Client, dataIndexType: string) {
     var papiClient = CommonMethods.getPapiClient(client);
-        var schema = await getSchemaFromAdal(papiClient,dataIndexType);
 
         var ui_adalRecord = await CommonMethods.getDataIndexUIAdalRecord(papiClient,client);
         var savedFields:string[] = [];
         if (ui_adalRecord[`${dataIndexType}_fields`]) {
         savedFields = ui_adalRecord[`${dataIndexType}_fields`];
         }
-    
-    let fields = schema.filter(f=>savedFields.includes(f.FieldID))
+    let schemaFields : SchemaFields = await getSchemaFields(papiClient,dataIndexType,savedFields);
 
-    await SaveOptionalValuesFromElastic(client, fields, dataIndexType)
+    await SaveOptionalValuesFromElastic(client, schemaFields, dataIndexType)
 
-    return { Fields: fields };
+    return { Fields: schemaFields };
 }
 
-function getFieldListFromElasticSearchMapping(mapping,prefix){
-    let fields:{FieldID:string,Type:string}[] = [];
-    for (let property in mapping) {
-        let value = mapping[property];
-        if(value["properties"]){
-            fields = fields.concat(getFieldListFromElasticSearchMapping(value["properties"],`${prefix}${property}.`));
-        }
-        else{
-            fields.push({FieldID:`${prefix}${property}`,Type:value["type"]});
-        }
-    }
-    return fields;
-}
+// function getFieldListFromElasticSearchMapping(mapping,prefix){
+//     let fields:{FieldID:string,Type:string}[] = [];
+//     for (let property in mapping) {
+//         let value = mapping[property];
+//         if(value["properties"]){
+//             fields = fields.concat(getFieldListFromElasticSearchMapping(value["properties"],`${prefix}${property}.`));
+//         }
+//         else{
+//             fields.push({FieldID:`${prefix}${property}`,Type:value["type"]});
+//         }
+//     }
+//     return fields;
+// }
 
-async function getSchemaFromElastic(client,dataIndexType){
-    var papiClient = CommonMethods.getPapiClient(client);
-    var distributorUUID = jwtDecode(client.OAuthAccessToken)['pepperi.distributoruuid'];
 
-    const response = await papiClient.addons.api.uuid("00000000-0000-0000-0000-00000e1a571c").sync().file("internal").func("get_mapping").post({type:dataIndexType})
-//    await callElasticSearchLambda(`${distributorUUID}/_mapping`, "GET", "");
-
-    var index_mapping = {}
-        
-    if(response[distributorUUID]){
-         if(response[distributorUUID]["mappings"]){
-             if(response[distributorUUID]["mappings"]["properties"])
-            index_mapping = response[distributorUUID]["mappings"]["properties"];
-        }
-    }
-
-    return getFieldListFromElasticSearchMapping(index_mapping,"");
-
-}
 
 // dataIndexType is "all_activities" or "transaction_lines"
-async function getSchemaFromAdal(papiClient: PapiClient, dataIndexType) {
-    let schemaFields: {FieldID:string,Type:string}[] = []
-    const fieldsFromAdal = (await papiClient.get(`/addons/data/schemes/${dataIndexType}`)).Fields;
-    for (let fieldName in fieldsFromAdal) {
-        schemaFields.push({FieldID: fieldName, Type: fieldsFromAdal[fieldName].Type})
+async function getSchemaFields(papiClient: PapiClient, dataIndexType,savedFields):Promise<SchemaFields> {
+    let schemaFields: SchemaFields = {};
+
+    const schema = await papiClient.get(`/addons/data/schemes/${dataIndexType}`);
+    const fields = schema.Fields;
+    for (let fieldID in fields) {
+        if(savedFields.includes(fieldID))
+            schemaFields[fieldID]= 
+            {
+                Type: fields[fieldID].Type
+            };
     }
     return schemaFields;
 }
 
-async function SaveOptionalValuesFromElastic(client, fields, dataIndexType) {
+async function SaveOptionalValuesFromElastic(client:Client, schemaFields : SchemaFields, dataIndexType:string) {
     var papiClient = CommonMethods.getPapiClient(client);
 
     const multiSelectFields = {
@@ -106,19 +97,21 @@ async function SaveOptionalValuesFromElastic(client, fields, dataIndexType) {
                              "Transaction.StatusName","Transaction.Type","Item.MainCategory"]
     }
 
-    for(var field of fields) {
-        if(multiSelectFields[dataIndexType].includes(field.FieldID)){
+    for(var fieldID in schemaFields) {
+        if(multiSelectFields[dataIndexType].includes(fieldID)){
+            schemaFields[fieldID].Type = "MultipleStringValues";
             let body = {
                 "size":"0",
                 "aggs" : {
                     "distinct_values" : {
-                        "terms" : { "field" : field.FieldID }
+                        "terms" : { "field" : fieldID }
                     }
                 }
             }
             const res = await papiClient.post(`/addons/shared_index/index/papi_data_index/search/${client.AddonUUID}/${dataIndexType}`,body);
             const distinct_values = res["aggregations"]["distinct_values"]["buckets"].map(x => x.key);
-            field["optionalValues"] = distinct_values;
+            
+            schemaFields[fieldID]["OptionalValues"] = distinct_values;
         }
     }
 }
